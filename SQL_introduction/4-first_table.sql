03e4e7f9b7b42851ce1ae66664e9e69a5c3d246a
Emahnny <3ma195@gmail.com>
2024-06-30 18:48:55 -0400

    fixng fiile 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
-Content: Create a table called first_table with the following attributes:
+-- Create a table called first_table with the following attributes:
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256)
