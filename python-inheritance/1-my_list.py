5a1710f36976deb05027469f98b83b2670bb4e78
Emahnny <3ma195@gmail.com>
2024-06-29 23:21:16 -0400

    adding whole ihnerit proyect

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 """
-Define the MyList class
+    Define the MyList class
 """
 
 class Mylist(list):
     """
-    MyList class that ihnerits the list
+        MyList class that ihnerits the list
+
+        Methods:
+            print_sorted: Print the sorted list
     """
     def print_sorted(self):
         """
-        Print a sorted list
+            Print a sorted list
         """
         print(sorted(self))
+
+
+
+
+        
